74d4ee9c3484f31c7da8d1c28e809574afb52cd3
luz maria <lu.maidacl@gmail.com>
2017-01-05 16:48:21 -0400

    Labo 4 is builted

---
+++ in-depth-expressions/js/MainController.js
@@ -0,0 +1,13 @@
+function MainController(){
+  // this.myArray = ['my', 'angular', 'app'];
+  // this.myArray = [];
+  // this.myObject = {
+  //   one: 'Key1',
+  //   two: 'Key2'
+  // };
+  this.text = 'Hello World!';
+}
+
+angular
+  .module('app')
+  .MainController('MainController', MainController);
---
+++ labo-4/js/TodoController.js
@@ -0,0 +1,34 @@
+function TodoController(){
+  this.newTodo = '';
+  this.list = [{
+    title: 'First todo item',
+    completed: true
+  },{
+    title: 'Second todo item',
+    completed: false
+  },{
+    title: 'Third todo item',
+    completed: true
+  }];
+
+  this.addTodo = function (){
+    this.list.unshift({
+      title: this.newTodo,
+      completed: false
+    });
+    this.newTodo = '';
+  };
+
+  this.removeTodo = function (item, index){
+    this.list.splice(index, 1);
+  };
+
+  this.getRemaining = function (){
+    return this.list.filter(function (item){
+      return !item.completed;
+    });
+  };
+}
+angular
+  .module('app')
+  .controller('TodoController', TodoController);
